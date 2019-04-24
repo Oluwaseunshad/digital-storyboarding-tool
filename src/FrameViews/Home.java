102075a64795c6650c177ac9cc0ff7bab3610b6c
Lydia Akuaku <lakuaku@bgsu.edu>
2019-04-23 21:27:00 -0400

    new branch created

--- src/FrameViews/Home.java
+++ src/FrameViews/Home.java
@@ -1,4 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package FrameViews;
+
+/**
+ *
+ * @author dakua
+ */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -8,12 +18,19 @@ public class Home implements ItemListener {
     final static String home = "Home";
     final static String createNewSb = "Create New StoryBoard";
     final static String trackReq = "Track Requirements";
-
+    static JPanel canvasPanel, toolsPanel, buttonsPanel;
+    static Canvas drawingArea;
+   // static ArrayList <String> tools = new ArrayList();
+   // static String tools [] = new String[] {"Square, Arrow, Oval, Line"};
+    static JButton saveButton, newButton,exitButton;
+    static JSplitPane split1;
+    static JSplitPane split2;
+    private int drawingArea_width = 200,drawingArea_height =200;
 
     public void addComponentToPane(Container pane) {
         //Put the JComboBox in a JPanel to get a nicer look.
         JPanel comboBoxPane = new JPanel(); //use FlowLayout
-        String comboBoxItems[] = { home, createNewSb, trackReq };
+        String comboBoxItems[] = {home, createNewSb, trackReq};
         JComboBox cb = new JComboBox(comboBoxItems);
         cb.setEditable(false);
         cb.addItemListener(this);
@@ -24,8 +41,40 @@ public class Home implements ItemListener {
         card1.add(new JTextField("Homepage", 20));
 
         JPanel card2 = new JPanel();
-        card2.add(new JTextField("Create New StoryBoard", 20));
-
+       // card2.setSize(300,300);
+      
+        saveButton = new JButton("Save");
+        newButton = new JButton("New");
+        exitButton = new JButton("Exit");
+        
+        buttonsPanel = new JPanel();
+       // buttonsPanel.setLayout(new GridLayout(2, 1));
+        buttonsPanel.add(saveButton);
+        buttonsPanel.add(newButton);
+        buttonsPanel.add(exitButton);
+        
+        drawingArea = new Canvas();
+       //// drawingArea.setSize(200,300);
+       // drawingArea.setMinimumSize(new Dimension(200,300));
+        drawingArea.setBackground(Color.green);
+        drawingArea.setLayout(new BorderLayout(6, 10));
+        drawingArea.setBorder(BorderFactory.createTitledBorder("Draw Here"));
+         
+        toolsPanel = new JPanel();
+      //  toolsPanel.setMinimumSize(new Dimension(100,100));
+        toolsPanel.setLayout(new BorderLayout(6, 10));
+        split1 = new JSplitPane();
+        split2 = new JSplitPane();
+        split1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, drawingArea, toolsPanel);
+        //split1.resetToPreferredSizes();
+      //  split1.setResizeWeight(1);
+        split2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, split1, buttonsPanel); 
+       // split2.resetToPreferredSizes();
+       split2.setResizeWeight(1);
+       // split2 .setMinimumSize(new Dimension(450,350));
+       
+        card2.add(split2);
+       
         JPanel card3 = new JPanel();
         card3.add(new JTextField("Track Requirements", 20));
 
@@ -38,6 +87,15 @@ public class Home implements ItemListener {
         pane.add(comboBoxPane, BorderLayout.WEST);
         pane.add(cards, BorderLayout.CENTER);
     }
+    
+   class Canvas extends JPanel{
+         @Override
+        public void paint(Graphics g){
+            super.paint(g);
+            
+            g.drawRect(10, 20, 10, 20);
+        }
+   }
 
     public void itemStateChanged(ItemEvent evt) {
         CardLayout cl = (CardLayout)(cards.getLayout());
